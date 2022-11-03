635c53967dd5fea3ac15a0bcbc014cc6effe4d14
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2022-11-03 16:35:31 +0100

    Update ex-1.cpp

--- lesson-3/ex-1.cpp
+++ lesson-3/ex-1.cpp
@@ -14,18 +14,18 @@ class Family
     public:
         Family()
         {
-            mother_name=BASIC_MNAME; 
-            father_name=BASIC_FNAME;
+            this->mother_name=BASIC_MNAME; 
+            this->father_name=BASIC_FNAME;
         }
         Family(string n1,string n2)
         {
-            mother_name=n1;
-            father_name=n2;
+            this->mother_name=n1;
+            this->father_name=n2;
         }
         void tellTheNames()
         {
-            cout<<"Mother's name is "<<mother_name<<endl;
-            cout<<"Father's name is "<<father_name<<endl;
+            cout<<"Mother's name is "<<this->mother_name<<endl;
+            cout<<"Father's name is "<<this->father_name<<endl;
         }
         
 };
@@ -38,23 +38,23 @@ class Habitat
     public:
         Habitat()
         {
-            habitat_type=BASIC_HABITAT;
-            has_fresh_water=true;
-            human_presence=false;
+            this->habitat_type=BASIC_HABITAT;
+            this->has_fresh_water=true;
+            this->human_presence=false;
         }
         Habitat(string ht,bool wa,bool hu)
         {
-            habitat_type=ht;
-            has_fresh_water=wa;
-            human_presence=hu;
+            this->habitat_type=ht;
+            this->has_fresh_water=wa;
+            this->human_presence=hu;
         }
         void showHabitatInformation()
         {
-            cout<<"The habitat type is: "<<habitat_type<<endl<<"It ";
-            if(has_fresh_water)cout<<"has ";
+            cout<<"The habitat type is: "<<this->habitat_type<<endl<<"It ";
+            if(this->has_fresh_water)cout<<"has ";
             else cout<<"doesn't have";
             cout<<"fresh water. Also, there ";
-            if(human_presence)cout<<"are ";
+            if(this->human_presence)cout<<"are ";
             else cout<<"aren't ";
             cout<<"humans around."<<endl;
         }
@@ -71,50 +71,50 @@ class Animal
     Animal()
     {
         cout<<"An animal was born!"<<endl;
-        species=BASIC_ATYPE;
-        name=BASIC_NNAME;
-        can_be_domesticated=false;
+        this->species=BASIC_ATYPE;
+        this->name=BASIC_NNAME;
+        this->can_be_domesticated=false;
     }
     Animal(string animal_type)
     {
         cout<<"An animal: "<<animal_type<<" was born!"<<endl;
-        species=animal_type;
-        name=BASIC_NNAME;
-        can_be_domesticated=false;
+        this->species=animal_type;
+        this->name=BASIC_NNAME;
+        this->can_be_domesticated=false;
         
     }
     Animal(string animal_type, string n)
     {
         cout<<"An animal: "<<animal_type<<" named "<<n<<" was born!"<<endl;
-        species=animal_type;
-        name=n;
-        can_be_domesticated=false;
+        this->species=animal_type;
+        this->name=n;
+        this->can_be_domesticated=false;
     }
     Animal(string animal_type, string n, Habitat h)
     {
         cout<<"An animal: "<<animal_type<<" named "<<n<<" was born!"<<endl;
-        species=animal_type;
-        name=n;
-        can_be_domesticated=false;
-        habitat=h;
+        this->species=animal_type;
+        this->name=n;
+        this->can_be_domesticated=false;
+        this->habitat=h;
     }
     Animal(string animal_type, string n, Family f, Habitat h,bool c)
     {
         cout<<"An animal: "<<animal_type<<" named "<<n<<" was born! And it even has family!"<<endl;
-        species=animal_type;
-        name=n;
-        family=f;
-        habitat=h;
-        can_be_domesticated=c;
+        this->species=animal_type;
+        this->name=n;
+        this->family=f;
+        this->habitat=h;
+        this->can_be_domesticated=c;
     }
     void showAnimalInformation()
     {
-        cout<<"The animal's name is: "<<name<<endl<<"It is of "<<species<<" species"<<endl;
+        cout<<"The animal's name is: "<<this->name<<endl<<"It is of "<<this->species<<" species"<<endl;
         cout<<"Its family: "<<endl;
-        family.tellTheNames();
-        habitat.showHabitatInformation();
+        this->family.tellTheNames();
+        this->habitat.showHabitatInformation();
         cout<<"It ";
-        if(can_be_domesticated)cout<<"can be domesticaded";
+        if(this->can_be_domesticated)cout<<"can be domesticaded";
         else cout<<"can't be domesticated";
         cout<<endl;
     }
