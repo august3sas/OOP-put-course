9c11234642f3615e75f062e8f3fb5e6ec7b86f55
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2022-12-08 16:01:23 +0100

    Create day-01.cpp

---
+++ lesson-7/day-01.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
+class Elf{
+    public:
+        int calories=0;
+        void addCalories(int x){calories+=x;}
+        void resetElf(){calories=0;}
+};
+class Herd{
+    public:
+        vector<Elf>elves;
+        void addElf(Elf e){elves.push_back(e);}
+        int retrieveMax(){
+        	int mx=0;
+        	for(int i=0;i<elves.size();i++){
+        		if(elves[i].calories>mx){
+        		mx=elves[i].calories;
+        		}
+        	}
+        	return mx;
+}
+
+};
+int main()
+{
+	Herd GroupOfElves;
+	Elf OneElf;
+	string x="";
+	do{
+		getline(cin,x);
+		if(x!=""){int temp=stoi(x);OneElf.addCalories(temp);}
+		else {GroupOfElves.addElf(OneElf);OneElf.resetElf();}
+	}while(cin);//CTRL-D to END
+	cout<<GroupOfElves.retrieveMax()<<endl;
+}
