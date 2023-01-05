80af1aaab494ac836d19bbd3f8ebde0cc87f757b
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2023-01-05 15:31:55 +0100

    Create ex-01.cpp

---
+++ lesson-09/ex-01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Game{
+	public:
+	virtual int result()=0;
+};
+class FakeFootballGame : public Game{
+	public:
+	int result(){return 1033;}
+};
+int main(){
+	FakeFootballGame game_05_01;
+	cout<<game_05_01.result()<<endl;
+	if (game_05_01.result()==1033){cout<<"This is a fake score";}else cout<<"This is a real score"<<endl;
+//	cout<<game_05_01.result()<<endl;
+}
