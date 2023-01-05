d482bae7d45bdbc3227efa3a0ef8aba4ed69301b
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2023-01-05 15:35:57 +0100

    Update ex-01.cpp

--- lesson-09/ex-01.cpp
+++ lesson-09/ex-01.cpp
@@ -6,11 +6,11 @@ class Game{
 };
 class FakeFootballGame : public Game{
 	public:
-	int result(){return 1033;}
+	int result(){return 10;}
 };
 int main(){
 	FakeFootballGame game_05_01;
 	cout<<game_05_01.result()<<endl;
-	if (game_05_01.result()==1033){cout<<"This is a fake score";}else cout<<"This is a real score"<<endl;
+	if (game_05_01.result()==10){cout<<"This is a fake score";}else cout<<"This is a real score"<<endl;
 //	cout<<game_05_01.result()<<endl;
 }
